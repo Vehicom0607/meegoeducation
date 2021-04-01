9fe59985071ae093de6c5a4731d04943c9af8958
Vehicom0607 <60050686+Vehicom0607@users.noreply.github.com>
2021-04-01 12:37:56 -0400

    Finished Top of Index

--- src/Pages/index/index.js
+++ src/Pages/index/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import MeeGoNavbar from "../../Components/Navbar/Navbar";
 import classes from './index.module.css'
+import {Col, Container, Image, Row} from "react-bootstrap";
 class IndexPage extends Component {
 
     componentDidMount() {
@@ -13,6 +14,19 @@ class IndexPage extends Component {
             <div>
                 <div className={classes.BgHeader}>
                     <MeeGoNavbar />
+                    <Container>
+                        <Row>
+                            <Col>
+                                <p className="text-white display-4 text-left">Learn Together,</p>
+                                <p className="text-white display-4 text-left">Grow Together.</p>
+                                <p className="text-white text-left">Enrichment courses with engaging teachers for a stimulating learning experience.</p>
+
+                            </Col>
+                            <Col>
+                                <Image alt="Man holding something idk" src="/Index/header1.png"/>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
             </div>
         );
