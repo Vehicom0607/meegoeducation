827d8e0361f7a08a87bd8a7e46649951d7e4ae17
Vehicom0607 <60050686+Vehicom0607@users.noreply.github.com>
2021-04-22 13:49:12 -0400

    Added more course card stuff.

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import classes from "./Courses.module.css";
 import MeeGoNavbar from "../../Components/Navbar/Navbar";
 import {Container} from "react-bootstrap";
+import CoursesCourseCard from "./CoursesCourseCard/CourseCard";
 
 
 class CoursesPage extends Component {
@@ -18,6 +19,9 @@ class CoursesPage extends Component {
                     <Container className={classes.ExtraPadding}>
                         <h1 className={classes.HeaderTitle}>Our Courses</h1>
                         <p className={classes.HeaderText}>Pandente itaque viam fatorum sorte tristissima, qua praestitutum erat eum vita et imperio spoliari, itineribus interiectis  iumenjor emensis venit Petobionem oppidum Noricorum.</p>
+                        <div style={{position: 'relative'}}>
+                            <CoursesCourseCard />
+                        </div>
                     </Container>
                 </div>
             </div>
--- src/Pages/Courses/CoursesCourseCard/CourseCard.js
+++ src/Pages/Courses/CoursesCourseCard/CourseCard.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import classes from './CourseCard.module.css'
 
 const CoursesCourseCard = () => {
     return (
-        <div>
-            
+        <div className={classes.Card}>
+            <div>
+                <img width="60" height="60" src="https://meegoimages.s3.us-east-2.amazonaws.com/writing1.png" />
+            </div>
+            <div>
+                bottom
+            </div>
         </div>
     );
 };
