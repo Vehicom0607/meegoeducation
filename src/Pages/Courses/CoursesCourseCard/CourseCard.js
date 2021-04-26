2e230a105c61d684cd0b9cabd7229e3ac1df90bc
Vehicom0607 <60050686+Vehicom0607@users.noreply.github.com>
2021-04-25 21:58:39 -0400

    Finished CourseCard. Need to make more and probably store the data serverside so it's easier to edit on like an admin page.

--- src/Pages/Courses/CoursesCourseCard/CourseCard.js
+++ src/Pages/Courses/CoursesCourseCard/CourseCard.js
@@ -4,11 +4,16 @@ import classes from './CourseCard.module.css'
 const CoursesCourseCard = () => {
     return (
         <div className={classes.Card}>
-            <div>
-                <img width="60" height="60" src="https://meegoimages.s3.us-east-2.amazonaws.com/writing1.png" />
+            <div className={classes.CardTop}>
+                <img width="60" height="60" src="https://meegoimages.s3.us-east-2.amazonaws.com/icon-1-blue%402x.png" />
+                <div>
+                    <p style={{fontWeight: '500', color: "#707070", fontSize: '18px', margin: '0'}}>English</p>
+                    <p>Pandente itaque viam fatorum sorte tristissima, qua praestitutum erat eum vita et imperio spoliari, itineribus interiectis  iumenjor emensis venit Petobionem oppidum Noricorum.</p>
+                </div>
             </div>
-            <div>
-                bottom
+            <div className={classes.CardBottom}>
+                <p>9 Courses</p>
+                <div className={classes.plus} />
             </div>
         </div>
     );
