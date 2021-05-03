5520cd59373dfc173179383910fe8c409c95f864
Vehicom0607 <60050686+Vehicom0607@users.noreply.github.com>
2021-05-03 11:26:02 -0400

    Added a InfoDisplay as a component and added some data displays on course.js.

---
+++ src/Components/InfoDisplay/InfoDisplay.js
@@ -0,0 +1,18 @@
+import classes from '../../Pages/Course/Course.module.css'
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+const InfoDisplay = props => {
+    return (
+        <p className={classes.CategoryText}>
+            <span style={{display: "inline-flex", width: '32px', height: '16px', justifyContent: 'center'}}>
+                <FontAwesomeIcon style={{color: '#2699FB'}} icon={props.icon} />
+            </span>
+            {props.children}
+        </p>
+    );
+};
+
+
+
+export default InfoDisplay;
--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -2,22 +2,23 @@ import React, {Component} from 'react';
 import classes from "./Course.module.css";
 import MeeGoNavbar from "../../Components/Navbar/Navbar";
 import {Col, Container, Row} from "react-bootstrap";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import 'font-awesome/css/font-awesome.min.css'
-import {faClock} from "@fortawesome/free-solid-svg-icons";
+import {faClock, faDollarSign, faStar, faUserFriends, faCalendarAlt} from "@fortawesome/free-solid-svg-icons";
+import InfoDisplay from "../../Components/InfoDisplay/InfoDisplay";
 
 class Course extends Component {
     render() {
-        console.log(this.props)
+        const course = this.props.courseData
+        const courseData = this.props.courseData.coursedata
         return (
             <div>
                 <div className={classes.BgGradient}>
                     <MeeGoNavbar />
                     <Container>
                         <div style={{display: 'flex', alignContent: 'flex-start'}}>
-                            <h2 className={classes.Title}>{this.props.courseData.title}</h2>
-                            <p className={classes.CategoryText} style={{padding: '0 25px', color: '#f5f5f5'}}>{this.props.courseData.category}</p>
-                            <p className={classes.CategoryText} style={{color: '#f5f5f5'}}>{this.props.courseData.courses} Courses</p>
+                            <h2 className={classes.Title}>{course.title}</h2>
+                            <p className={classes.CategoryText} style={{padding: '0 25px', color: '#f5f5f5'}}>{courseData.category}</p>
+                            <p className={classes.CategoryText} style={{color: '#f5f5f5'}}>{courseData.courses} Courses</p>
                         </div>
                     </Container>
                 </div>
@@ -26,11 +27,15 @@ class Course extends Component {
                         <Col>
                             <img className="p-5" alt="random for now" src="https://source.unsplash.com/random/450x300" />
                         </Col>
-                        <Col>
+                        <Col className="my-5">
+                            <InfoDisplay icon={faClock}>50 minutes per class</InfoDisplay>
+                            <InfoDisplay icon={faDollarSign}>{`${courseData.price} for ${courseData.courses} Courses`}</InfoDisplay>
+                            <InfoDisplay icon={faStar}>{courseData.prerequisite}</InfoDisplay>
+                            <InfoDisplay icon={faUserFriends}>{courseData.teacher}</InfoDisplay>
+                            <InfoDisplay icon={faCalendarAlt}>{`Recommended ages: ${courseData.minAge}-${courseData.maxAge}`}</InfoDisplay>
                             <p className={classes.CategoryText + " my-5"}>
                                 {this.props.courseData.description}
                             </p>
-                            <p className={classes.CategoryText}><FontAwesomeIcon style={{color: '#2699FB'}} icon={faClock} /> 50 minutes per class</p>
                         </Col>
                     </Row>
                 </Container>
